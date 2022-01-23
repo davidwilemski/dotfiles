39db03b27e8ce2139fb561079bfb05a54e064f5c
David Wilemski <david@wilemski.org>
2022-01-23 17:03:01 -0600

    Autoinstall iso generator for optiplex 2060 hardware
    - ssd on sda
    - 4g swap (on 16g mem)
    - zfs
    
    Based on this script with modifications for my hardware: https://github.com/Xe/nixos-configs/tree/d6f4f31b4/media/autoinstall-alrest

---
+++ nix/media/autoinstall-optiplex2060/configuration.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+let
+  fetchKeys = username:
+    (builtins.fetchurl "https://github.com/${username}.keys");
+in {
+  imports = [
+    #installer-only ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.devNodes = "/dev/disk/by-partuuid";
+
+  networking.interfaces.enp1s0.useDHCP = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "install";
+  networking.firewall.enable = false;
+
+  users.users.root.openssh.authorizedKeys.keyFiles = [ (fetchKeys "davidwilemski") ];
+
+  services.openssh.enable = true;
+  users.mutableUsers = false;
+}
