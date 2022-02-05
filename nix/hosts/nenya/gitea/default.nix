81fcbd0d07d66b3eda9cf6e07a73118d623f1127
David Wilemski <david@wilemski.org>
2022-02-04 22:04:14 -0600

    gitea: enable dumps and metrics

--- nix/hosts/nenya/gitea/default.nix
+++ nix/hosts/nenya/gitea/default.nix
@@ -3,6 +3,19 @@
 {
   services.gitea = {
     enable = true;
+    dump.enable = true;
     disableRegistration = true;
+
+    domain = "nenya";
+
+    settings = {
+      metrics = {
+        ENABLED = true;
+        # TODO Add secret auth token
+      };
+      server = {
+        SSH_DOMAIN = "nenya"; # supposedly defaults to %(DOMAIN)s but not working with top-level domain setting?
+      };
+    };
   };
 }
