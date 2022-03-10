5336341f305bcbafc879afc565b72c2796429e7d
David Wilemski <david@wilemski.org>
2022-03-10 09:08:24 -0600

    fixes for tmux on macos reordering PATH

--- nix/common/home-manager/home-environment.nix
+++ nix/common/home-manager/home-environment.nix
@@ -8,5 +8,15 @@
   # environment variables
   home.sessionVariables = {
     EDITOR = "nvim";
+    PATH = "$HOME/bin:$PATH";
   };
+
+  # ensure macos path_helper doesn't overwrite PATH
+  # See this about macos path_helper https://superuser.com/questions/544989/does-tmux-sort-the-path-variable
+  programs.fish.loginShellInit = ''
+    if test -n "$TMUX"
+      and test -f "/etc/profile"
+      set PATH "$HOME/bin:$PATH" # needed to ensure ~/bin is placed first in PATH
+    end
+  '';
 }
