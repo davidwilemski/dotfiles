10eb2ac83f1a328b5ae173b6870c792a4ae65532
David Wilemski <david@wilemski.org>
2024-01-15 18:16:17 +0000

    reduce default cache max-age

--- nix/hosts/barahir/caddy/default.nix
+++ nix/hosts/barahir/caddy/default.nix
@@ -36,7 +36,7 @@ in
       hostName = "o.davidwilemski.com";
       listenAddresses = [ "206.55.186.217" ];
       extraConfig = ''
-        header ?Cache-Control "max-age=3600"
+        header ?Cache-Control "max-age=300"
         encode zstd gzip
         reverse_proxy narya:3030
       '';
